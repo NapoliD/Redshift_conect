757620e70b98f4694fc3c60df39a6718681c00f4
Diego <53539772+NapoliD@users.noreply.github.com>
2023-03-09 12:30:20 -0300

    Create Redshift_store_fast.py

---
+++ Redshift_store_fast.py
@@ -0,0 +1,23 @@
+
+import boto3
+import psycopg2
+
+
+client = boto3.client('redshift',region_name = '',aws_access_key_id = ',aws_secret_access_key = '')
+cluster_creds = client.get_cluster_credentials(DbUser='',DbName='',ClusterIdentifier='',AutoCreate=False)
+
+
+print('connect psycopg2')
+con = psycopg2.connect(host='redshift-.redshift.amazonaws.com',port='0000',user=cluster_creds['DbUser'],password=cluster_creds['DbPassword'],database='base')
+print(con)
+cursor=con.cursor()
+
+
+print('copy to redshift')
+
+sql_COPY_query = f"""COPY aprende.vw_aprende_comms FROM 's3://bucket/.csv' IAM_ROLE 'arn:aws:iam::' DELIMITER '|' IGNOREHEADER as 1 CSV;"""
+print(sql_COPY_query)
+cursor.execute(sql_COPY_query)
+con.commit()
+con.close()
+print('ok sql_COPY_query')
